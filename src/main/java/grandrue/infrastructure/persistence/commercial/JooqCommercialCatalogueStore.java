ada1e0b04862444d05eacc9f7572d6b6bd40cafb
swangune <swangune@gmail.com>
2026-09-17 23:09:39 +0100

    refactor: migrate catalogue publication exception namespace

--- src/main/java/grandrue/infrastructure/persistence/commercial/JooqCommercialCatalogueStore.java
+++ src/main/java/grandrue/infrastructure/persistence/commercial/JooqCommercialCatalogueStore.java
@@ -1,5 +1,6 @@
 package grandrue.infrastructure.persistence.commercial;
 
+import grandrue.commercial.CataloguePublicationException;
 import mainstreet.application.TrustedPlatformExecutionContext;
 import mainstreet.commercial.*;
 import org.jooq.DSLContext;
