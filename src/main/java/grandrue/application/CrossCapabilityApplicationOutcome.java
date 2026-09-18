a74c70e504368edf1ad436c1e1f3382c89a2bd30
swangune <swangune@gmail.com>
2026-09-18 02:31:47 +0100

    refactor: migrate cross-capability application outcome namespace

--- src/main/java/grandrue/application/CrossCapabilityApplicationOutcome.java
+++ src/main/java/grandrue/application/CrossCapabilityApplicationOutcome.java
@@ -1,6 +1,8 @@
-package mainstreet.application;
+package grandrue.application;
 
 import grandrue.application.ApplicationOutcomeClassification;
+import mainstreet.application.ApplicationRequestIdentity;
+import mainstreet.application.CommittedProgressReference;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
