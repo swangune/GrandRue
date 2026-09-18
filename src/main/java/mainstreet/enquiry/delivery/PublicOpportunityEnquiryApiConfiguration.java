47639e111df7ca3e537fe1094a2f1eadf078c706
swangune <swangune@gmail.com>
2026-09-18 08:42:46 +0100

    refactor: migrate PublicOpportunityEnquiryRequirements namespace

--- src/main/java/mainstreet/enquiry/delivery/PublicOpportunityEnquiryApiConfiguration.java
+++ src/main/java/mainstreet/enquiry/delivery/PublicOpportunityEnquiryApiConfiguration.java
@@ -1,6 +1,7 @@
 package mainstreet.enquiry.delivery;
 
 import grandrue.enquiry.delivery.PublicEnquirySubmissionAdmissionAuthority;
+import grandrue.enquiry.delivery.PublicOpportunityEnquiryRequirements;
 import mainstreet.enquiry.EnquirySubmissionApplicationService;
 import mainstreet.publication.*;
 import mainstreet.publication.delivery.PublicOpportunityRouteScopeAuthority;
