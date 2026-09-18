3a66e4717666d7897a3d3807d3bd99680137b0d6
swangune <swangune@gmail.com>
2026-09-18 18:01:55 +0100

    refactor: migrate MerchantLocationProgressAffinityReadPort to GrandRue

--- src/main/java/mainstreet/merchantprofile/MerchantLocationExposureChoiceReadPort.java
+++ src/main/java/mainstreet/merchantprofile/MerchantLocationExposureChoiceReadPort.java
@@ -1,5 +1,7 @@
 package mainstreet.merchantprofile;
 
+import grandrue.merchantprofile.MerchantLocationProgressAffinityReadPort;
+
 import grandrue.application.MerchantScope;
 
 import java.util.Optional;
