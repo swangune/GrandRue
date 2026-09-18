f3aa1981a9448c2c0bf87883fb354dd79e280742
swangune <swangune@gmail.com>
2026-09-18 04:47:53 +0100

    refactor: migrate MerchantAccountEstablishedPublicationWorker namespace

--- src/main/java/grandrue/application/MerchantAccountEstablishedPublicationWorker.java
+++ src/main/java/grandrue/application/MerchantAccountEstablishedPublicationWorker.java
@@ -1,7 +1,8 @@
-package mainstreet.application;
+package grandrue.application;
 
 import grandrue.merchantaccount.MerchantAccountEstablishedOccurrence;
 import grandrue.merchantaccount.MerchantAccountEstablishedPublicationSource;
+import mainstreet.application.StandingFreeEventReactionContract;
 import mainstreet.semantic.event.EventReactionContractDefinition;
 import mainstreet.semantic.event.EventReactionContractRegistrySnapshot;
 import mainstreet.semantic.event.EventReactionIdentity;
